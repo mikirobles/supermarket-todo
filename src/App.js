86da47a7247bea5e9f15f0feab781e8c43b85b89
Micael Robles <mrobles@litebox.ai>
2019-02-27 13:51:23 -0300

    More hooks

--- src/App.js
+++ src/App.js
@@ -1,116 +1,101 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import api from "./api";
 import "./App.css";
 import ListItem from "./components/ListItem";
 import AddItem from "./components/AddItem";
 
-class App extends React.Component {
-  state = {
-    isEditing: false,
-    initialLoad: true,
-    error: null,
-    items: []
-  };
-  
+function App() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [fetching, setFetching] = useState(false);
 
-  componentDidMount() {
-    this.fetchItems();
-  }
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-  fetchItems = async () => {
+  const fetchItems = async () => {
     try {
+      setFetching(true);
       const items = await api.fetchItems();
-      this.setState({
-        items,
-        initialLoad: false
-      });
+      setItems(items);
     } catch (e) {
-      this.setState({
-        error: "An error has occurred while fetching the items.",
-        initialLoad: false
-      });
+      setError("An error has occurred while fetching the items.");
     }
+    setFetching(false);
+    setInitialLoad(false);
   };
 
-  switchModal = () => {
-    this.setState(({ isEditing }) => ({
-      isEditing: !isEditing
-    }));
+  const switchModal = () => {
+    setIsEditing(!isEditing);
   };
 
-  addItem = async item => {
-    const { items } = this.state;
+  const addItem = async item => {
+    setIsEditing(false);
+    const oldItems = [...items];
+    if (fetching) return;
+    setFetching(true);
     try {
-      this.setState({
-        items: [...items, item],
-        isEditing: false
-      });
+      setItems([...items, item]);
       const newItems = await api.addItem(item);
-      this.setState({
-        items: newItems
-      });
+      setItems(newItems);
     } catch (e) {
-      this.setState({
-        items
-      });
+      setItems(oldItems);
     }
+    setFetching(false);
   };
 
-  removeItem = async item => {
-    const { items } = this.state;
-    if (typeof item.id === "undefined") {
+  const removeItem = async item => {
+    if (fetching || typeof item.id === "undefined") {
       return;
     }
+    const oldItems = [...items];
+    setFetching(true);
     try {
-      this.setState({
-        items: [...items].filter(({ id }) => id !== item.id)
-      });
+      setItems([...items].filter(({ id }) => id !== item.id));
       api.removeItem(item);
     } catch (e) {
-      this.setState({
-        items
-      });
+      setItems(oldItems);
     }
+    setFetching(false);
   };
 
-  render() {
-    const { items, error, isEditing, initialLoad } = this.state;
-    return (
-      <div className="App">
-        <div className="container">
-          <h1>Supermarket List</h1>
-          {items.length ? (
-            <span className={"item-count"}>{items.length} items</span>
-          ) : initialLoad ? (
-            <span className={"item-count"}>Loading...</span>
-          ) : (
-            <span className={"item-count"}>List is empty</span>
-          )}
-          <ul className={"list"}>
-            {items.map(({ name, id }, index) => (
-              <ListItem
-                key={id || name + index}
-                label={name}
-                disabled={typeof id === "undefined"}
-                onDelete={() => this.removeItem({ name, id })}
-              />
-            ))}
-          </ul>
-          <button
-            autoFocus
-            onClick={this.switchModal}
-            className={"btn primary add-item"}
-          >
-            Add item
-          </button>
-          {error && <strong>{error}</strong>}
-          {isEditing && (
-            <AddItem onCancel={this.switchModal} onSubmit={this.addItem} />
-          )}
-        </div>
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Supermarket List</h1>
+        {items.length ? (
+          <span className={"item-count"}>{items.length} items</span>
+        ) : initialLoad ? (
+          <span className={"item-count"}>Loading...</span>
+        ) : (
+          <span className={"item-count"}>List is empty</span>
+        )}
+        <ul className={"list"}>
+          {items.map(({ name, id }, index) => (
+            <ListItem
+              key={id || name + index}
+              label={name}
+              disabled={typeof id === "undefined" || fetching}
+              onDelete={() => removeItem({ name, id })}
+            />
+          ))}
+        </ul>
+        <button
+          autoFocus
+          onClick={switchModal}
+          className={"btn primary add-item"}
+        >
+          Add item
+        </button>
+        {error && <strong>{error}</strong>}
+        {isEditing && !error && (
+          <AddItem onCancel={switchModal} onSubmit={addItem} />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
